3051dc69a905447f195d7fe24a2d2f2552767986
Saiesh <saiesh_naik@persistent.com>
2023-02-16 11:47:40 +0530

    Images and components folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,3 +16,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import App from "./App"
+
+// ReactDOM.render(<App />, document.getElementById("root"))
